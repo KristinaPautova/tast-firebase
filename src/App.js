bbd228a3162c6a8fd11bd2283e9d5f648d977b4c
KristinaPautova <christie.pautova@gmail.com>
2020-08-10 12:42:21 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,102 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from 'react';
+import * as firebase from 'firebase'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      email: '',
+      password: '',
+      hasAccount: false,
+      name: '',
+      key: '',
+      value: '',
+      data: {}
+      }
+    }
+    componentDidMount(){
+      const db = firebase.database();
+      const surname = db.ref('surname')
+      surname.on('value', (elem) => this.setState({
+        data: elem.val()
+      }))
+    }
+    handleChange = ({target : { value, id}}) => {
+      this.setState({
+        [id] : value,
+      })
+    }
+  
+
+  createAccount = () => {
+    const {email,password} = this.state;
+    // firebase.auth().createUserWithEmailAndPassword(email,password)
+    // .catch(error => console.log(error))
+
+    firebase.auth().signInWithEmailAndPassword(email, password)
+    .then( response => {
+       this.setState({hasAccount: true})
+    })
+    .catch(error => console.log(error))
 }
 
-export default App;
+  sendData = () => {
+    const {key, value} = this.state;
+    const db = firebase.database();
+    db.ref(key).push(value);
+    alert('yur data was written to db')
+  }
+
+  render(){
+    const { hasAccount,  data ,name } = this.state;
+    return(
+      <div>
+        {
+         hasAccount ? 
+         (
+          <div>
+            <input 
+               type = 'text'
+               id = "key"
+               placeholder = "enter key"
+               onChange = {this.handleChange}/>
+            <input 
+               type = 'text'
+               id = "value"
+               placeholder = "enter value"
+               onChange = {this.handleChange}/> 
+            <input 
+               type = "submit" 
+               onClick = {this.sendData}/> 
+               {
+                 Object.keys(data).map(elem => (
+                   <div>
+                     {data[elem]}
+                   </div>
+                 ))
+               }           
+          </div>
+        )
+        :
+        (<div className = 'login_block'>
+          <input 
+             type = "text" 
+             id = "email" 
+             placeholder = "email" 
+             onChange = {this.handleChange}/>
+          <input 
+             type = "password" 
+             id = "password" 
+             placeholder = "password" 
+             onChange = {this.handleChange}/>
+          <input 
+             type = "submit" 
+             onClick = {this.createAccount}/>
+        </div>
+        )
+      }
+      </div>
+    )
+  }
+}
+
--- src/index.js
+++ src/index.js
@@ -3,6 +3,20 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import * as firebase from 'firebase'
+
+
+
+const firebaseConfig = {
+  apiKey: "AIzaSyD_xSCSC0E7AbWDvGWDZDsm4nc8qnFzTzs",
+  authDomain: "test-firebase-f58c7.firebaseapp.com",
+  databaseURL: "https://test-firebase-f58c7.firebaseio.com",
+  projectId: "test-firebase-f58c7",
+  storageBucket: "test-firebase-f58c7.appspot.com",
+  messagingSenderId: "48006969289",
+  appId: "1:48006969289:web:a56cc40a8505f795195a18"
+}
+firebase.initializeApp(firebaseConfig);
 
 ReactDOM.render(
   <React.StrictMode>
@@ -11,7 +25,4 @@ ReactDOM.render(
   document.getElementById('root')
 );
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
 serviceWorker.unregister();
